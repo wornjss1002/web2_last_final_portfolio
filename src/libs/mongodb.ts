09ca07e061bfa58992c73d1192e76ae37b4f1202
wornjss1002 <wornjs1002@naver.com>
2024-12-12 00:29:24 +0900

    asd

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -2,19 +2,10 @@ import mongoose from 'mongoose'
 
 export default async function connectMongoDB() {
   try {
-    const uri = process.env.MONGODB_URI
-    if (!uri) {
-      throw new Error('MONGODB_URI가 환경 변수에 설정되지 않았습니다.')
-    }
-
-    if (mongoose.connection.readyState === 1) {
-      return
-    }
-
-    await mongoose.connect(uri)
-    console.log('MongoDB 연결 성공')
+    await mongoose.connect(process.env.MONGODB_URI as string)
+    console.log('Connected to MongoDB')
   } catch (error) {
-    console.error('MongoDB 연결 실패:', error)
-    throw error
+    console.error(error)
   }
+  console.log('MongoDB URI:', process.env.MONGODB_URI);
 }
